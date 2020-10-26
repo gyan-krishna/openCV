54ce414f5baff786ba2ef49de9fcd55cae52028b
gyan-krishna <45621173+gyan-krishna@users.noreply.github.com>
2020-10-26 10:00:14 +0530

    update on 26/10/2020

--- examplePrograms/5. image enhancement in spatial domain/imageEnhancement.py
+++ examplePrograms/5. image enhancement in spatial domain/imageEnhancement.py
@@ -11,28 +11,40 @@ neg = 1 - img
 
 ##############################################################################
 # log transformation on image, done on gray scale image
-# formula :: opPix = ScalingConst * log(1 + ipPix)
+# formula :: opPix = ScalingConst * log(ipPix)
 # ipPix - input pixel image
 # opPix - output pixel image
-# ScalingConst = 255/( log(1+max_input_pixel_value) )
-# adding 1 to input pixel, since log0 = inf, and want to avoid that
+# ScalingConst = 255/( log(max_input_pixel_value) )
+# have to remove 0 from input matrix since, log(0) = inf
 
 #removing 0 from the image
 grayNonZero = np.where( gray==0 , 1, gray)
+
 #modified formula since 0 has been removed from the array already
 ScalingConst = 255 / np.log(np.max(grayNonZero))
 logImg = ScalingConst * np.log(grayNonZero)
+
 logImg = logImg.astype(np.uint8)
 
 ##############################################################################
+#power log transformation/gamma transformation
+
+gamma = 3.5
+gammaImg= np.array( 255*(img/255) ** gamma, dtype='uint8')
+
+##############################################################################
+
 
 
 cv2.imshow("original image", img)
 cv2.imshow("negative image", neg)
 cv2.imshow("gray scale image", gray)
 cv2.imshow("log transformation image", logImg)
+cv2.imshow("gamma transformation image - "+str(gamma), gammaImg)
 
 cv2.imwrite("negativeImg.jpg", neg)
 cv2.imwrite("log transformation.jpg", logImg)
+cv2.imwrite("gamma transformation image - "+str(gamma)+".jpg", gammaImg)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
