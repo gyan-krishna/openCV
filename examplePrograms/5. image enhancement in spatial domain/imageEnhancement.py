40af574ad09a9045e683d8b3d345f36cd3134d6b
gyan-krishna <45621173+gyan-krishna@users.noreply.github.com>
2020-10-27 11:38:26 +0530

    update on 27/10/2020

--- examplePrograms/5. image enhancement in spatial domain/imageEnhancement.py
+++ examplePrograms/5. image enhancement in spatial domain/imageEnhancement.py
@@ -4,6 +4,7 @@ import numpy as np
 
 img = cv2.imread("minions_raw.jpg",1)
 gray = cv2.imread("minions_raw.jpg",0)
+agera = cv2.imread("agrea.jpg",0)
 
 #creating negative image
 neg = 1 - img
@@ -33,14 +34,30 @@ gamma = 0.5
 gammaImg= np.array( 255*(gray/255) ** gamma, dtype='uint8')
 
 ##############################################################################
+#Piecewise - Linear Transformation Function -
 
+def pixVal(pix, r1, s1, r2, s2):
+    if (0 <= pix and pix <= r1):
+        return (s1/r1)*pix
+    elif (r1 < pix and pix <= r2):
+        return (s2 - s1)/(r2-r1) * (pix - r1) + s1
+    else:
+        return (255 - s1)/(255 - r1) * (pix - r2) + s2
+    
+r1, s1 = 100, 0
+r2, s2 = 160, 180
+
+pixVector = np.vectorize(pixVal)
+contrastStretched = pixVector(agera, r1, s1, r2, s2)
 
 
 cv2.imshow("original image", img)
+cv2.imshow("agera image", agera)
 cv2.imshow("negative image", neg)
 cv2.imshow("gray scale image", gray)
 cv2.imshow("log transformation image", logImg)
 cv2.imshow("gamma transformation image - "+str(gamma), gammaImg)
+cv2.imshow("piece wise linear transformation", contrastStretched)
 
 cv2.imwrite("negativeImg.jpg", neg)
 cv2.imwrite("log transformation.jpg", logImg)
