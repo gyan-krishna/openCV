67d3a227b95d155dfeecd5eeb0f2c93fb5333dd1
gyan-krishna <45621173+gyan-krishna@users.noreply.github.com>
2020-10-31 15:07:26 +0530

    update on 29/10/2020

--- examplePrograms/9. image arithmetics/imageArithmetic.py
+++ examplePrograms/9. image arithmetics/imageArithmetic.py
@@ -37,7 +37,7 @@ cv2.imshow("diff",diff)
 
 cv2.imwrite("WeightedSum.jpg",weightedSum)
 cv2.imwrite("NonWeightedSum.jpg",sum)
-cv2.imwrite("WeightedSum.jpg",weightedSum)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
