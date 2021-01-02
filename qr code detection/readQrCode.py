933c1c0a4991188acfd54f61f3cf4831e42f38aa
gyan-krishna <45621173+gyan-krishna@users.noreply.github.com>
2021-01-02 12:08:25 +0530

    qr code detection

--- qr code detection/readQrCode.py
+++ qr code detection/readQrCode.py
@@ -11,22 +11,28 @@ import cv2    #library to procress
 import pyzbar #library to procress bar/qr codes
 #from pyzbar.pyzbar import decode
 
-#qrcode.png
-#barcode.jpg
 
-img = cv2.imread("barcode.jpg")
+sampleFiles = ["qrcode.jpg", "barcode.jpg"]
+sampleOPFiles = ["qrcodeOP.png", "barcodeOP.jpg"]
+fileNo = 1
+
+img = cv2.imread(sampleFiles[fileNo])
 codes = pyzbar.pyzbar.decode(img)
 
 for code in codes:
     x1, y1, w, h = code.rect
     x2, y2 = w+x1, h+y1
     
-    cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
     codeData = code.data.decode("utf-8")
     codeType = code.type
+    
+    cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
+    img = cv2.putText(img, codeType, (x1+20, y1+40), cv2.FONT_HERSHEY_SIMPLEX , 1, (0,0,255), 2, cv2.LINE_AA)
+    img = cv2.putText(img, codeData, (x1+20, y1+100), cv2.FONT_HERSHEY_SIMPLEX , 1, (0,0,255), 2, cv2.LINE_AA)
     print("found {} with data :: {}".format(codeType, codeData))
 
 cv2.imshow("image",img)
+cv2.imwrite(sampleOPFiles[fileNo], img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
         
